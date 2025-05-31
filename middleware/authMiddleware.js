092e052a31626f36559fad27525a58cc2f908e2a
nithin-maddu <nithinmaddu13@gmail.com>
2025-05-31 09:59:22 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,22 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "mytokken"
-function authMiddleware(req,res,next){
-const authHeader = req.headers.authorization;
-if(!authHeader) return res.send('missing token ')
+const JWT_SECRET = "mytokken";
 
-    const token = authHeader.split(' ')[1];
-try{
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
 
-    const decode = jwt.verify(token,JWT_SECRET)
-    req.userId = decode.id;
-    next()
-}catch(err){
-res.send(err)
+  if (!authHeader) {
+    return res.status(401).json({ error: "Missing token" });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
 }
 
-}
-
-module.exports = authMiddleware
+module.exports = authMiddleware;
