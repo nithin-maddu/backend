7aeb9543dff45292377b332c59923e09c88ecdcf
nithin-maddu <nithinmaddu13@gmail.com>
2025-05-31 10:26:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 4000;
 const mongoose = require('mongoose');
-const authMiddleware = require('./middleware/authMiddleware')
-const cors = require('cors')
+const cors = require('cors');
 
-const {
-  createProduct,
-  getAllProducts,
-  getProductById,
-  updateProduct,
-  deleteProduct
-} = require('./controllers/productController');
+const { createStudent, readall } = require('./controllers/studentController');
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require('./controllers/productController');
+const { registerUser, loginUser } = require('./controllers/authController'); // ✅ Add this
+const authMiddleware = require('./middleware/authMiddleware');
 
-app.use(express.json())
-//Db format
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb+srv://nithinmaddu13:Maddu@cluster0.mzdenph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=>{
-    console.log('db connected') 
-})
-.catch((err)=>{
-    console.log(err)
-})
+  .then(() => console.log('db connected'))
+  .catch(err => console.log(err));
 
+app.get('/', authMiddleware, (req, res) => {
+  res.end("hi from express");
+});
 
-app.use(cors())
+// Auth routes ✅
+app.post('/api/auth/register', registerUser);
+app.post('/api/auth/login', loginUser);
 
-app.get('/',(req,res)=>{
-res.end("hi from express")
-})
+// Student APIs
+app.post('/api/students', createStudent);
+app.get('/api/students', readall);
 
-//apis CRUD
-
-
+// Product APIs
 app.post('/api/products', createProduct);
 app.get('/api/products', getAllProducts);
 app.get('/api/products/:id', getProductById);
 app.put('/api/products/:id', updateProduct);
 app.delete('/api/products/:id', deleteProduct);
 
-
-
-app.listen(port, ()=>{
-    console.log('server is running')
-})
+app.listen(port, () => {
+  console.log('server is running');
+});
